aa384d14864caf493c4eb0f399e3e50dd3f9e78d
SankaraVishnu629 <sankaravishnu629@gmail.com>
2024-08-02 12:07:12 +0530

    feat: this is my first program

---
+++ reverseString.java/src/App.java
@@ -0,0 +1,17 @@
+class App {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
